386e448458e45ef1d2a7a63cf5bc52b7b5a8fa80
Jennifer041995 <ms21100@ucad.edu.sv>
2025-08-17 16:38:15 -0600

    proyecto angular

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,15 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { EmpleadosComponent } from './empleados/empleados';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [EmpleadosComponent],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('prueba');
+  alumno: string = "Jennifer";
+  edad: number = 30;
 }
